00458b534c76d2e856611d7dcb8d75adf4d22b9b
PeterFiorentino <peterfiorentino@pursuit.org>
2020-07-20 16:21:35 -0400

    reworked the recursive tribonacci

--- Recursion/nthTrib.js
+++ Recursion/nthTrib.js
@@ -1,4 +1,7 @@
 var tribonacci = function(n) {
-    if (n < 3) return 1    
-    return tribonacci(n-1) + tribonacci(n-2)
+    if (n == 0) return 0;
+    if (n == 1) return 1
+    if (n == 2) return 1;
+    
+    return tribonacci(n-3) + tribonacci(n-2) + tribonacci(n - 1);
 };
