d5d46c6c083a8134cf8b53658c71d992b696e0b0
PeterFiorentino <peterfiorentino@pursuit.org>
2020-10-20 13:33:46 -0400

    solved the Lucky Sum problem on codingJS

---
+++ CodingJS/luckySum.js
@@ -0,0 +1,27 @@
+// Given 3 int values, a b c, return their sum. 
+// However, if one of the values is 13 then it does not count towards the sum and values to its right do not count. 
+// So for example, if b is 13, then both b and c do not count.
+
+// Examples
+
+// luckySum(1, 2, 3) → 6
+// luckySum(1, 2, 13) → 3
+// luckySum(1, 13, 3) → 1
+
+function luckySum(a, b, c){
+    let sum = 0;
+    if(a === 13){
+      return sum
+    }
+    if(b === 13){
+      sum = a
+      return sum
+    }
+    if(c === 13){
+      sum = a + b
+      return sum
+    }
+    
+    sum = a + b + c
+    return sum
+  }
